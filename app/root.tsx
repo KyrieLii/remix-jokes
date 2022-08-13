8c0787262eba3d5bab1b6e2c7def41d2dd890b9c
limao2021 <limao2021@bytedance.com>
2022-08-14 00:14:49 +0800

    feat: meta

--- app/root.tsx
+++ app/root.tsx
@@ -1,34 +1,50 @@
-import { LiveReload, Outlet, Links } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import { LiveReload, Outlet, Links, Meta } from "@remix-run/react";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 
 import globalStylesUrl from "./styles/global.css";
 import globalMediumStylesUrl from "./styles/global-medium.css";
 import globalLargeStylesUrl from "./styles/global-large.css";
 
 export const links: LinksFunction = () => {
-    return [
-      {
-        rel: "stylesheet",
-        href: globalStylesUrl,
-      },
-      {
-        rel: "stylesheet",
-        href: globalMediumStylesUrl,
-        media: "print, (min-width: 640px)",
-      },
-      {
-        rel: "stylesheet",
-        href: globalLargeStylesUrl,
-        media: "screen and (min-width: 1024px)",
-      },
-    ];
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
+};
+
+export const meta: MetaFunction = () => {
+  const description = `Learn Remix and laugh at the same time!`;
+  return {
+    charset: "utf-8",
+    description,
+    keywords: "Remix,jokes",
+    "twitter:image": "https://remix-jokes.lol/social.png",
+    "twitter:card": "summary_large_image",
+    "twitter:creator": "@remix_run",
+    "twitter:site": "@remix_run",
+    "twitter:title": "Remix Jokes",
+    "twitter:description": description,
   };
+};
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <Meta />
         <title>Remix: So great, it's funny!</title>
         <Links />
       </head>
